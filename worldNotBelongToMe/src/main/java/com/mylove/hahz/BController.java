9157eae9e8236507e392cb4fb8871c3677e62182
lichuang <lichuang@shxgroup.net>
2020-07-20 17:32:31 +0800

    I'm just a little code...

--- worldNotBelongToMe/src/main/java/com/mylove/hahz/BController.java
+++ worldNotBelongToMe/src/main/java/com/mylove/hahz/BController.java
@@ -10,4 +10,10 @@ public class BController {
     private String getB(){
         return "789545474897984513215465424651";
     }
+
+    public void testC(){
+        String a = getA();
+        String b = getB();
+        System.out.println(a+b);
+    }
 }
