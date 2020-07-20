5254cd356c61b8aa291fdc3760a6e1044828af63
lichuang <lichuang@shxgroup.net>
2020-07-20 17:05:32 +0800

    commit new ctr class.

---
+++ worldNotBelongToMe/src/main/java/com/mylove/hahz/BController.java
@@ -0,0 +1,13 @@
+package com.mylove.hahz;
+
+public class BController {
+
+    private String getA(){
+        return "zxczxczczcasdasdadsasfqwrqeqwe";
+    }
+
+
+    private String getB(){
+        return "789545474897984513215465424651";
+    }
+}
